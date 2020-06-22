2e8e8246fa2ebc478258df184e9ce0a5fec536b2
hlmnicholson <nicholson.helena@gmail.com>
2020-06-22 16:57:23 +0200

    backend finished

--- src/api/logs.js
+++ src/api/logs.js
@@ -4,11 +4,10 @@ const LogEntry = require('../models/LogEntry');
 
 const router = Router();
 
-router.get('/', (req, res, next) => {
+router.get('/', async (req, res, next) => {
   try {
-    res.json({
-      message: 'globe',
-    });
+    const entries = await LogEntry.find();
+    res.json(entries);
   } catch (error) {
     next(error);
   }
@@ -19,10 +18,8 @@ router.post('/', async (req, res, next) => {
   try {
     const logEntry = new LogEntry(req.body);
     const createdEntry = await logEntry.save();
-    console.log(createdEntry);
+    res.json(createdEntry);
   } catch (error) {
-    // eslint-disable-next-line no-console
-    console.log(error.name);
     if (error.name === 'ValidationError') {
       res.status(422);
     }
